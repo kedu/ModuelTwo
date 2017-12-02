156edd5a8936d14efea9710e06dbaa35c2120ee6
lkb_shidaiwanwang <shidaiwangwang_lkb@163.com>
2017-12-02 09:01:37 +0800

    初始化

---
+++ 模块一/模块一/LKBVC.h
@@ -0,0 +1,16 @@
+//
+//  LKBVC.h
+//  模块一
+//
+//  Created by Apple on 2017/12/2.
+//  Copyright © 2017年 李康滨,工作qq:1218773641. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKBVC : UIViewController
+
+
++ (NSString*)author ;
+
+@end
